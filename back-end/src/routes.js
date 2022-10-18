e8f4a6857db0f3afcb9d4109ed6ff97df10d1827
Veríssimo Casas <Verissimo0073@gmail.com>
2022-10-17 23:29:47 -0400

    add routes

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import HelloController from "./controllers/HelloController";
+
+import UsersController from "./controllers/UsersController";
 
 const routes = new Router();
 
-routes.get("/hello", HelloController.index);
+routes.get("/users", UsersController.index);
+routes.get("/users/:id", UsersController.show);
+routes.post("/users", UsersController.create);
+routes.put("/users/:id", UsersController.update);
 
 export default routes;
