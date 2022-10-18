02aa985279a6bde3457ce83c657c3b5967392c3a
Veríssimo Casas <Verissimo0073@gmail.com>
2022-10-18 11:31:06 -0400

    Creating CRUD routes for Users

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -8,5 +8,6 @@ routes.get("/users", UsersController.index);
 routes.get("/users/:id", UsersController.show);
 routes.post("/users", UsersController.create);
 routes.put("/users/:id", UsersController.update);
+routes.delete("/users/:id", UsersController.delete);
 
 export default routes;
