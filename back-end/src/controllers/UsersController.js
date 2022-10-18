87eeee00ba29ff98dc30021cb4a1d54061afa9a5
Veríssimo Casas <Verissimo0073@gmail.com>
2022-10-18 11:32:32 -0400

    Update UsersControllers

--- back-end/src/controllers/UsersController.js
+++ back-end/src/controllers/UsersController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import { hashPassword } from "../services/auth";
 
 class UsersController {
   async index (request, response) {
@@ -11,7 +12,18 @@ class UsersController {
   }
 
   async show (request, response) {
+    try {
+      const { id } = request.params;
+      const user = await User.findById(id);
 
+      if (!user) {
+        return response.status(404).json({ message: "User not found." });
+      }
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(500).json({ error: "Internal server error." });
+    }
   }
 
   async create (request, response) {
@@ -22,8 +34,14 @@ class UsersController {
       if (user) {
         return response.status(422).json({ message: `User ${email} already exists` });
       }
+      
+      const hashedPassword = await hashPassword(password);
 
-      const newUser = await User.create({ email, password });
+      const newUser = await User.create({ 
+        email, 
+        password: hashedPassword 
+      });
+
       return response.status(201).json(newUser);
       
     } catch (error) {
@@ -32,11 +50,43 @@ class UsersController {
   }
 
   async update (request, response) {
+    try {
+      const { id } = request.params;
+      const { email, password } = request.body;
+      const user = await User.findById(id);
 
+      if (!user) {
+        return response.status(404).json({ message: "User not found." });
+      }
+
+      const hashedPassword = await hashPassword(password);
+
+      const updatedUser = await User.updateOne({
+        email,
+        password: hashedPassword
+      });
+
+      return response.status(200).json();
+    } catch (error) {
+      
+    }
   }
 
-  async detroy (request, response) {
+  async delete (request, response) {
+    try {
+      const { id } = request.params;
+      const user = await User.findById(id);
 
+      if (!user) {
+        return response.status(404).json({ message: "User not found." });
+      }
+
+      await User.deleteOne({ _id: id });
+
+      return response.status(200).json();
+    } catch (error) {
+      return response.status(500).json({ error: "Internal server error." });
+    }
   }
 
 }
